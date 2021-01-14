1817397b532fece3e066953f10b3094c88153566
Kilo <uron.shala@gmail.com>
2021-01-14 18:09:41 +0100

    Login page fixed

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width= device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>wesell</title>
+
+    </head>
+    <body>
+        <?php include('header.php');?>
+
+        <!--Construction-->
+        <div class="constructionsite">
+            <div class="constructionimage">
+            </div>
+        </div>
+
+
+            <!--footer-->
+        <?php include('footer.php');?>
+    </body>
+</html>
---
+++ loginform.php
@@ -0,0 +1,18 @@
+        <!---form-->
+        <div class="contactbody">
+            <div class="contactf">
+                <h1>Login</h1>
+                <div class="contactformB">
+                <form>
+                <div class="form-control">
+                    <input type="text" name="name" placeholder="Username" required>
+                </div>
+                <div class="form-control">
+                    <input type="text" name="password" placeholder="Password" required>
+                </div>
+                <input type="submit" value="Kyqu" class="btn-sendc">
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
