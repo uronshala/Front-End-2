dfa46162cc407acb5dbb7ae0233be5513d0321d6
Kilo <uron.shala@gmail.com>
2021-01-19 18:24:09 +0100

    Fixed non-responsive div's to Responsive.

--- config.php
+++ config.php
@@ -1,23 +1,16 @@
-<section class="formcase">
-        <div class="form-build grid">
-            <div class="build-first-text">
-            <h1>Dëshironi të konfiguroni PC-n tuaj?</h1>
-            <p>Ne si kompani , me profesionistet tane ne staf , kemi kënaqesin te shohim idete e juaja per konfigurimet e ndryshme te PC-ve.I mirepresim kerkesat e juaja per konfigurime , me respekt Wesell.</p>
-            </div>
-            <div class="buildform cardF">
-                <h2>Kontakto me ne</h2>
-                <form>
-                    <div class="form-control">
-                        <input type="text" name="name" placeholder="Emri" required>
-                    </div>
-                    <div class="form-control">
-                        <input type="text" name="lastname" placeholder="Mbiemri" required>
-                    </div>
-                    <div class="form-control">
-                        <input type="email" name="email" placeholder="Emaili" required>
-                    </div>
-                    <input type="submit" value="Dergo" class="btn-send">
-                </form>
+<!---kontakt--->
+<form>
+    <div class="container">
+        <div class="contact-box">
+            <div class="left">
+                <div class="right">
+                    <h2>Contact</h2>
+                    <input type="text" class="field" placeholder="Name" required>
+                    <input type="text" class="field" placeholder="Surname" required>
+                    <input tpye="emaili" class="field" placeholder="Email" required>
+                    <button type="submit" name="submit" class="btn-send">Send</button>
+                </div>
             </div>
         </div>
-    </section>
+    </div>
+</form>
--- footer.php
+++ footer.php
@@ -1,18 +1,23 @@
-<html>
-<body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>wesell</title>
+
+</head>
+<!----footer-->
 <footer>
-          <div class="footer">
-              <h3>wesell</h3>
-              <p>wesell is a company that sells tech parts</p>
-              <ul class="buttons">
-                  <li><a href="index.html">Home</a></li>
-                  <li><a href="products.html">Products</a></li>
-                  <li><a href="about.html">Rreth Nesh</a></li>
-                  <li><a href="contact.html">Kontakti</a></li>
-              </ul>
-          </div>
-          <div class="div footer-bottom">
-              <p>Copyright &copy;2021 Wesell.</p>
-          </div>
-      </footer>
-</html>
+    <div class="footer">
+        <h3>Wesell</h3>
+        <ul class="buttons">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="products.html">Products</a></li>
+            <li><a href="about.html">Rreth Nesh</a></li>
+            <li><a href="contact.html">Kontakti</a></li>
+        </ul>
+    </div>
+    <div class="footer-bottom">
+        <p>Copyright &copy;2021 Wesell.</p>
+    </div>
+</footer>
