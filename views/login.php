8f8c66325fcc86aa1e42f9e14245a1b8e83d1f35
Kilo <uron.shala@gmail.com>
2021-02-05 02:38:30 +0100

    Changes on files , Products fullfiled.

--- views/login.php
+++ views/login.php
@@ -10,12 +10,12 @@
             <h1>Login</h1>
             <form method="post">
                 <div class="txt_field">
-                    <input type="text" required>
+                    <input type="text">
                     <span></span>
                     <label>Username</label>
                 </div>
                 <div class="txt_field">
-                    <input type="password" required>
+                    <input type="password" >
                     <span></span>
                     <label>Password</label>
                 </div>
@@ -29,17 +29,17 @@
             <h1>Create your Account</h1>
             <form method="post">
                 <div class="txt_field">
-                    <input type="text" required>
+                    <input type="text">
                     <span></span>
                     <label>Firstname</label>
                 </div>
                 <div class="txt_field">
-                    <input type="text" required>
+                    <input type="text">
                     <span></span>
                     <label>Lastname</label>
                 </div>
                 <div class="txt_field">
-                    <input type="password" required>
+                    <input type="password">
                     <span></span>
                     <label>Password</label>
                 </div>
